2a36e6feeeec279e732b62e561f1fd2839468969
ESMAT001 <esmat10.niazi@gmail.com>
2021-10-04 12:09:54 +0430

    wait prop added

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const puppeteer = require('puppeteer');
 
-const takePhoto = async ({ url = "https://www.google.com/" } = {}) => {
+const takePhoto = async ({ url = "https://www.google.com/", wait = null } = {}) => {
   console.log(url)
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const page = await browser.newPage();
-  await page.setViewport({ width: 1366, height: 768});
+  await page.setViewport({ width: 1366, height: 768 });
   await page.goto(url, { waitUntil: 'load', timeout: 0 });
-  // await page.waitForTimeout(60 * 1000)
+
+  if (wait) {
+    wait = parseInt(wait);
+    console.log(`waiting for ${wait}sec`)
+    await page.waitForTimeout(wait * 1000)
+  }
   await page.screenshot({ path: 'example1.png' });
 
   await browser.close();
